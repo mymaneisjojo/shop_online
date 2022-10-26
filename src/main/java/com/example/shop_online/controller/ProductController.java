33406572adca78083b2568584ffe4a15aa917efe
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-10-26 17:04:58 +0700

    "update order"

--- src/main/java/com/example/shop_online/controller/ProductController.java
+++ src/main/java/com/example/shop_online/controller/ProductController.java
@@ -12,10 +12,10 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-//    @GetMapping("/all")
-//    public ResponseEntity<?> getAll(){
-//        return ResponseEntity.ok(productService.getAll());
-//    }
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(productService.getAll());
+    }
 
     @PostMapping("/add")
     public ResponseEntity<?> addProduct(@RequestBody ProductDto productDto){
