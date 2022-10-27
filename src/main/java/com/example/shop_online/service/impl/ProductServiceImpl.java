caf49a9504ff6e77a20e7ce7a4aca50610f6289c
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-10-27 13:36:43 +0700

    "delete order "

--- src/main/java/com/example/shop_online/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/shop_online/service/impl/ProductServiceImpl.java
@@ -9,10 +9,12 @@ import com.example.shop_online.repository.ProductRepository;
 import com.example.shop_online.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
