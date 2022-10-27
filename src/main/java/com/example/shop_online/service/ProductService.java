cbd01af376f11d6dd2ad9f70ce6fe0a9d2f4066d
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-10-27 13:41:06 +0700

    "delete order "

--- src/main/java/com/example/shop_online/service/ProductService.java
+++ src/main/java/com/example/shop_online/service/ProductService.java
@@ -7,8 +7,8 @@ import com.example.shop_online.models.response.MessageResponse;
 import java.util.List;
 
 public interface ProductService {
-    public List<Product> getAll();
-    public MessageResponse add(ProductDto productDto);
-    public MessageResponse update(ProductDto productDto, int id);
-    public MessageResponse delete(int id);
+     List<Product> getAll();
+     MessageResponse add(ProductDto productDto);
+     MessageResponse update(ProductDto productDto, int id);
+     MessageResponse delete(int id);
 }
